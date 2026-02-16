346784409dceffb14a854abeeb4054ebf6890e4c
sandesh wakade <sunilwakade301@gmail.com>
2026-02-16 20:06:13 +0530

    Add files via upload

---
+++ -v_+v.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	printf("Enter number :");
+	scanf("%d",&a);
+	
+	if(a>0)
+	{
+		pirntf("Number is -ve");
+	}
+	else
+	{
+		printf("Number is +ve");
+	}
+	
+}
