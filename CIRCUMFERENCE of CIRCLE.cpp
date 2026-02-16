f9ba125ca4f01e7d28d30d05a892a5a7e26ffc03
sandesh wakade <sunilwakade301@gmail.com>
2026-02-16 20:07:59 +0530

    Add files via upload

---
+++ CIRCUMFERENCE of CIRCLE.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	float r,c,a; 
+	printf("Enter radius :");
+	scanf("%f",&r);
+	
+	a=3.14*r*r;
+	c=2*3.14*r;
+	
+	printf("AREA=%f",a);
+	printf("\nCIRCUMFERENCE of CIRCLE4=%f",c);
+}
